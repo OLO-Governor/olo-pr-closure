8390830c1ea6b85bc9f20b0802ab64d4d8c2bebf
Kenneth Williams <keni.williams@outlook.com>
2026-04-24 14:40:34 +0100

    OPRC-9: register GitHub signature middleware

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 from presentation.routes import router  # after env is loaded
+from presentation.middleware.github_signature import GitHubSignatureMiddleware
 
 # Load environment variables
 load_dotenv()
 
 app = FastAPI()
+
+app.add_middleware(GitHubSignatureMiddleware)
 
 app.include_router(router)
